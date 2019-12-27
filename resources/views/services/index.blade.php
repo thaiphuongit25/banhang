979ea519968ece3a3ee4604da135a5bbda57f324
Duy Tang <duyth1993@gmail.com>
2019-12-27 22:40:45 +0700

    Service show and index

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -9,6 +9,43 @@
     @include('services.menu_left')
 </div>
 <div id="body-right">
+    <div class="middle-page">
+        <div style="border: 1px solid #DDD;-moz-border-radius: 8px;-webkit-border-radius: 8px; background-color:#fff;">
+            <div style="font-size:14px; font-weight:bolder; padding:6px 0 8px 16px; color:#027AC7;">
+                Dịch vụ
+            </div>
+            <div style="padding:12px 6px 12px 12px;">
+              @foreach ($services as $service)
+                <div class="service">
+                    <div class="img-service">
+                        <a href="{{ route('services.show', ['service' => $service->id]) }}">
+                          <img alt="" src="https://thegioiic.com/upload/medium/2515.jpg">
+                        </a>
+                    </div>
+                    <div class="info-service">
+                        <div class="title-service">
+                            <h2 style="padding: 0;margin: 0">
+                              <a href="{{ route('services.show', ['service' => $service->id]) }}">
+                                {{ $service->title }}
+                              </a>
+                            </h2>
+                        </div>
+                        <div class="desc-service">
+                            {!! Str::words(strip_tags($service->content), 50, '...')  !!}
+                        </div>
+                    </div>
+                    <div class="detail-service">
+                        <a href="{{ route('services.show', ['service' => $service->id]) }}">Chi tiết</a>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+              @endforeach
+                <div style="margin-top: 20px;margin-left: 20px;width: 90%;float: left;text-align: center;">
+
+                </div>
+            </div>
+        </div>
+    </div>
     @include('services.menu_right')
 </div>
 @endsection
