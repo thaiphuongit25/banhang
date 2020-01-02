79a61963de06e53f551f0d8fcc1190227fbe2d38
phuong nguyen <phuong.nguyen@linkbal.vn>
2020-01-02 15:32:19 +0700

    check order

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -70,7 +70,7 @@
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label require">Số lượng</label>
                 <div class="col-sm-10">
-                    <input type="input" name="qunatity" class="form-control" />
+                    <input type="input" name="quantity" class="form-control" />
                 </div>
             </div>
             <div class="form-group row">
