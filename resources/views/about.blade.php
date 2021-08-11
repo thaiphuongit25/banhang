76850ea9b2b1a36c28e16661ae626e443f2bc696
Nguyen Thai Phuong <phuong.nguyen@linkbal.vn>
2021-08-11 16:11:59 +0700

    fix about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,10 +4,15 @@
 	<div class="container">
 			<h3 class="heading-agileinfo">Über uns<span>Dienstleistungen für Haut, Nägel und Schönheit</span></h3>
 				<div class="about-bott-right">
-					 <h5>Wer wir sind</h5>
-					 <p>Masagni dolores eoquie int Basmodi temporant, ut laboreas dolore magnam aliquam kuytase uaeraquis autem vel eum iure reprehend.Unicmquam eius, Basmodi temurer sehsMunim.Masagni dolores eoquie int Basmodi temporant, ut laboreas dolore magnam aliquam kuytase uaeraquis autem vel eum iure reprehend.</p>
+					 <p>Herzlich willkommen auf unserer HomePage
+DER LUXUS TRAUMHAFT SCHÖNER NÄGEL BEGINNT BEREITS BEIM BETRETEN UNSERES STUDIOS. BESUCHEN SIE UNS – LASSEN SIE SICH VERWÖHNEN
+Schöne gepflegte Fingernägel und Hände werten Ihr Äußeres auf, Sie fühlen sich wohler. Ganz gleich wo Sie sind, beim Ausgehen – Tanzen – Sport Beruf und Arbeit, können Sie Ihre Hände her zeigen. Immer wieder werden unsere Kundinnen auf Ihre schönen gepflegten Fingernägel angesprochen. Durch aktuelle Fortbildungsseminare, behandeln wir Ihre Fingernägel nach dem neusten Stand der Technik. Zum Nagel Design benutzen wir Air-Brush und Flexbrush Lack. Mit Air-Brush-Design gehören Nägel zu den exklusivsten Attributen der weiblichen Schönheit. Die kleinen Kunstwerke, mit Spritzpistole und Flexbrush Lack auf die Nägel gebannt, verleihen schönen Nägeln den besonderen Kick. Der eigenen Phantasie sind keine Grenzen gesetzt. Für jeden Anlass und jede Gelegenheit finden wir für Sie ein passendes Motiv. Der Einheitslook gehört der Vergangenheit an.!!!</p>
 				</div>
+
 				<div class="clearfix"> </div>
 			</div>
+			<div class="map_w3layouts_agile">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2486.3501335778833!2d6.623430515197859!3d51.45172762259401!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b8bce22ab8b9d5%3A0x65b8e6825ebd7b99!2sSteinstra%C3%9Fe%202D%2C%2047441%20Moers%2C%20Germany!5e0!3m2!1sen!2s!4v1628259267077!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+</div>
 </div>
 @endsection
